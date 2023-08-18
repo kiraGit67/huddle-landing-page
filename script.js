6e7f543544f5d67b9c18743348b7967613d03e0b
kiraGit67 <kirsten@sais.de>
2023-08-18 11:44:59 +0200

    add changeMenuVisible method

--- script.js
+++ script.js
@@ -8,4 +8,10 @@ createApp({
       isTooltipVisible: false,
     };
   },
+  methods: {
+    changeMenuVisible() {
+      console.log(this.isMenuVisible);
+      this.isMenuVisible = !this.isMenuVisible;
+    },
+  },
 }).mount("#app");
